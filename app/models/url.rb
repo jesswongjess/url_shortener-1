0667cb9d3dbdfa41b51f8aa089bfbb525aecebb3
Michelle  Ler Hsin Yee <MichelleLer@Michelles-MacBook-Pro.local>
2017-05-12 10:01:12 +0800

    tried capybara but not working

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,12 +1,8 @@
-require 'uri'
-
 class Url < ActiveRecord::Base
 
-  validates :long_url, presence: true, uniqueness: true, allow_blank: false
-  #validates :long_url, uniqueness: true
-  validates :short_url, presence: true, allow_blank: true
+  validates_presence_of :long_url, :short_url
   validate :is_valid_url
-  validates :long_url, :format => URI::regexp(%w(http https))
+  after_initialize :shorten
 
 	before_create do 
 		self.short_url = shorten
@@ -14,13 +10,19 @@ class Url < ActiveRecord::Base
 
     def shorten
     # Write a method here
-        return ((0..9).to_a + ("a".."z").to_a + ("A".."Z").to_a).sample(7).join
+        return ((0..9).to_a + ("a".."z").to_a + ("A".."Z").to_a).sample(7).join unless nil
     end
 
-    # def retrieve_short_url(proper_long_url)
-    # 	Url.is_valid_url
-    # 	return Url.shorten
-    # end	
+    def self.retrieve_short_url(proper_long_url)
+        url = Url.find_by(long_url: long_url)
+
+    	  if url.nil? 
+          return nil
+        else 
+         return url.short_url
+        end 
+    end	
+    
     private
     def is_valid_url
     #TODO upgrade validations
--- spec/views/urls/index.html.erb_spec.rb
+++ spec/views/urls/index.html.erb_spec.rb
@@ -3,3 +3,12 @@ require 'rails_helper'
 # RSpec.describe "urls/index.html.erb", type: :view do
 #   pending "add some examples to (or delete) #{__FILE__}"
 # end
+
+RSpec.describe "urls/index.html.erb", type: :feature do
+  # pending "add some examples to (or delete) #{__FILE__}"
+  it "has a link to the #new template" do
+    visit "/urls"
+    click_link("New URL")
+    expect(page.current_path).to eq("/urls/new")
+  end
+end
